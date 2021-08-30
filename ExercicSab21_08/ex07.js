09254c09518bcdc0a4e1eac7a7f037de1e4386b9
Thiago Vieira <74151323+thiagovieira7@users.noreply.github.com>
2021-08-30 16:16:09 -0300

    Update ex07.js

--- ExercicSab21_08/ex07.js
+++ ExercicSab21_08/ex07.js
@@ -1,3 +1,13 @@
 const prompt = require("prompt-sync")();
 /* 7- Faça um programa que tenha uma função chamada voto () que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições: */
 
+let ano = prompt("Informe o ano de nascimento: ");
+let verificaAno = 2021 - ano;
+//console.log(verificaAno);
+if (verificaAno < 16) {
+    console.log(verificaAno + " ANOS" + " \nVOTO NEGADO");
+} else if (verificaAno => 16 && verificaAno <= 17) {
+    console.log(verificaAno + " ANOS" + " \nVOTO OPCIONAL");
+} else {
+    console.log(verificaAno + " ANOS" + " \nVOTO OBRIGATORIO");
+}
