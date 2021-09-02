16e2f131f82487c5da7aa5d1d5a6fc5db5db7522
Thiago Vieira <74151323+thiagovieira7@users.noreply.github.com>
2021-09-02 16:03:47 -0300

    update

--- Projetos_Curso/Proj_03/02proj03Rasc.js
+++ Projetos_Curso/Proj_03/02proj03Rasc.js
@@ -1,5 +1,6 @@
 /* crie um programa onde jogadores joguem um dado e tenham resultados aleatórios.
 Perguntar quantas rodadas você quer fazer
+Perguntar quantos jogadores vão jogar;
 Criar um objeto pra cada jogador com nome e número tirado
 Guarda todos os objetos em uma lista
 Ordenar esses objetos, sabendo que o vencedor tirou o maior número
--- Projetos_Curso/Proj_03/armazInfoJog.js
+++ Projetos_Curso/Proj_03/armazInfoJog.js
@@ -1,6 +1,12 @@
 // Criar um objeto pra cada jogador com nome e número tirado;
 //Guarda todos os objetos em uma lista;
 
+const prompt = require("prompt-sync")();
+
+let jogadores = +prompt("Quantos jogadores irão participar? ");
+
+
+
 let listJogs = {};
 const pessoas = [
     {
@@ -9,3 +15,18 @@ const pessoas = [
     },
 ];
 
+
+for (let i = 0; i <= jogadores; i++) {
+    console.log("Rodada");
+    let jogador = {
+        nome: prompt("digite um nome: "),
+        dado: "",
+    };
+    console.log(jogador.nome);
+}
+
+let lista = [];
+lista.push(prompt("digite algo: "));
+//lista.push(prompt("digite algo: "));
+//lista.push(prompt("digite algo: "));
+console.log(lista);
