7b4fee07a185366b7859a4d3e291fc73efc9d7c3
Thiago Vieira <74151323+thiagovieira7@users.noreply.github.com>
2021-09-08 01:50:34 -0300

    update

---
+++ Exercic_Pratica/28_08/ex01.js
@@ -0,0 +1,5 @@
+/* ⦁	Exercício – Treino: Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração, multiplicação e divisão desses valores.
+ */
+
+const prompt = require("prompt-sync")();
+
