2482c610337514ab16c72feded83611fa067e049
Thiago Vieira <74151323+thiagovieira7@users.noreply.github.com>
2021-09-10 02:18:38 -0300

    update

--- Projetos_Curso/Proj_04/funcVotacao.js
+++ Projetos_Curso/Proj_04/funcVotacao.js
@@ -1,17 +1,19 @@
 const prompt = require("prompt-sync")();
 
-function autorizaVoto(anoNascimento) {
-    let idade = 2021 - anoNascimento;
+function votacao(autorizacao, voto) {
+    console.log("escolha o numero do candidato"),
+    
+
+     
+    
 
     if ((idade == 16) & (idade < 18)) {
         return "VOTO OPCIONAL";
     } else if (idade >= 18 && idade < 70) {
         return "VOTO OBRIGATORIO";
     } else {
-        return "VOTO NEGADO";
+        return "VOTO OPCIONAL";
     }
 }
 
-let anoUser = +prompt("Informe o seu ano de nascimento: ");
-let idade_usuario = autorizaVoto(anoUser);
 console.log(`${idade_usuario}`);
